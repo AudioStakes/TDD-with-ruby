3b663efb31da243bc16b4869b891d0f04b1b64a6
AudioStakes <daisuke.sato@enishi-tech.com>
2020-03-01 22:56:30 +0900

    Equality for All

--- test_money.rb
+++ test_money.rb
@@ -4,11 +4,16 @@ require './dollar'
 Minitest::Unit.autorun
 
 class TestMoney < Minitest::Unit::TestCase
-  def test_money
+  def test_multiplication
     five = Dollar.new 5
     product = five.times(2)
     assert_equal 10, product.amount
     product = five.times(3)
     assert_equal 15, product.amount
+  end
+
+  def test_equality
+    assert Dollar.new(5).equals(Dollar.new(5))
+    assert !Dollar.new(5).equals(Dollar.new(6))
   end
 end
