0146813e215a72d265ba632cdd7e540e03649dfc
AudioStakes <daisuke.sato@enishi-tech.com>
2020-03-07 23:58:26 +0900

    Remove subclasses from test code

--- money.rb
+++ money.rb
@@ -10,4 +10,16 @@ class Money
     && money.class == self.class
   end
   alias == equals
+
+  def self.dollar(amount)
+    Dollar.new(amount)
+  end
+
+  def self.franc(amount)
+    Franc.new(amount)
+  end
+
+  def times(multiplier)
+    raise 'System Error: method missing'
+  end
 end
--- test_money.rb
+++ test_money.rb
@@ -1,31 +1,32 @@
 require 'minitest'
 require './dollar'
 require './franc'
+require './money'
 
 Minitest::Unit.autorun
 
 class TestMoney < Minitest::Unit::TestCase
   def test_multiplication
-    five = Dollar.new 5
+    five = Money.dollar 5
     product = five.times(2)
-    assert_equal Dollar.new(10), product
+    assert_equal Money.dollar(10), product
     product = five.times(3)
-    assert_equal Dollar.new(15), product
+    assert_equal Money.dollar(15), product
   end
 
   def test_equality
-    assert Dollar.new(5).equals(Dollar.new(5))
-    assert !Dollar.new(5).equals(Dollar.new(6))
-    assert Franc.new(5).equals(Franc.new(5))
-    assert !Franc.new(5).equals(Franc.new(6))
-    assert !Franc.new(5).equals(Dollar.new(5))
+    assert Money.dollar(5).equals(Money.dollar(5))
+    assert !Money.dollar(5).equals(Money.dollar(6))
+    assert Money.franc(5).equals(Money.franc(5))
+    assert !Money.franc(5).equals(Money.franc(6))
+    assert !Money.franc(5).equals(Money.dollar(5))
   end
 
   def test_franc_multiplication
-    five = Franc.new 5
+    five = Money.franc 5
     product = five.times(2)
-    assert_equal Franc.new(10), product
+    assert_equal Money.franc(10), product
     product = five.times(3)
-    assert_equal Franc.new(15), product
+    assert_equal Money.franc(15), product
   end
 end
