398aecce094c9e1e6c3e6c6a4242bef51f91ce5c
AudioStakes <daisuke.sato@enishi-tech.com>
2020-03-11 23:27:22 +0900

    Preparing for different currency calculations

--- test_money.rb
+++ test_money.rb
@@ -1,7 +1,6 @@
 require 'minitest'
-require './dollar'
-require './franc'
 require './money'
+require './bank'
 
 Minitest::Unit.autorun
 
@@ -23,5 +22,13 @@ class TestMoney < Minitest::Unit::TestCase
   def test_currency
     assert_equal "USD", Money.dollar(1).currency()
     assert_equal "CHF", Money.franc(1).currency()
+  end
+
+  def test_simple_addition
+    five = Money.dollar(5)
+    sum = five.plus(five)
+    bank = Bank.new
+    reduced = bank.reduce(sum, "USD")
+    assert_equal Money.dollar(10), reduced
   end
 end
